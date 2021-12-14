8f7b3e3e79d94427825869ecc692c4c5ba1051da
Cesar Bassani <caobassani@gmail.com>
2021-12-13 22:28:10 -0300

    Implementando ouvir

--- pessoa.py
+++ pessoa.py
@@ -13,7 +13,8 @@ class Pessoa():
     def comer(self, comida):
         print(f'Estou comendo {comida}')    
 
-
+    def ouvir(self, ouvindo):
+        print(f'Estou ouvindo {ouvindo}')
 
 # livia = Pessoa('Livia', 49, 'xxx.xxx.xxx-xx', 'F', '7199999','Rua Tal')
 # livia.repetir(f'Oi, sou a livia, eu tenho {livia.idade} anos e moro na {livia.logradouro}'
@@ -25,8 +26,12 @@ sexo = input('Insira o seu sexo: ')
 telefone = input('Insira o seu telefone: ')
 logradouro = input('Insira o seu logradouro: ')
 comida = input('O que você está comendo: ')
+ouvindo = input('O que você está ouvindo: ')
 
 usuario = Pessoa(nome, idade, cpf, sexo, telefone, logradouro)
 frase = f'Oi meu nome é {usuario.nome}, eu tenho {usuario.idade} anos e moro na {usuario.logradouro}'
 usuario.repetir(frase)
 usuario.comer(comida)
+usuario.ouvir(ouvindo)
+
+# Continuar com (Fazer uma herança).
