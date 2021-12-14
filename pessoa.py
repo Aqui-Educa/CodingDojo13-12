7539836579ee25315a350ffad4deb054365dee47
Victor <victorfalmeida12@gmail.com>
2021-12-13 21:28:20 -0300

    input usuario

--- pessoa.py
+++ pessoa.py
@@ -8,8 +8,18 @@ class Pessoa():
         self.logradouro = logradouro
 
     def repetir(self, msg):
-        print(msg)
+        print(f'{self.nome}: {msg}')
 
-livia = Pessoa('Livia', '49', 'xxx.xxx.xxx-xx', 'F' '7199999','Rua Tal')
-print(livia)
 
+# livia = Pessoa('Livia', 49, 'xxx.xxx.xxx-xx', 'F', '7199999','Rua Tal')
+# livia.repetir(f'Oi, sou a livia, eu tenho {livia.idade} anos e moro na {livia.logradouro}'
+
+nome = input('Insira o seu nome: ')
+idade = input('Insira o sua idade: ')
+cpf = input('Insira o seu cpf: ')
+sexo = input('Insira o seu sexo: ')
+telefone = input('Insira o seu telefone: ')
+logradouro = input('Insira o seu logradouro: ')
+
+usuario = Pessoa(nome, idade, cpf, sexo, telefone, logradouro)
+usuario.repetir(f'Oi meu nome é {usuario.nome}, eu tenho {usuario.idade} anos e moro na {usuario.logradouro}')
